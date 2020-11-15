c92754e1857c35925b2c0c35ef8b37e377b1ec64
Mateusz <bmatteo@interia.pl>
2020-11-15 12:04:48 +0100

    commit

--- script.js
+++ script.js
@@ -5,6 +5,47 @@ haslo = haslo.toUpperCase();
 var ukryte_haslo = "";
 var dlugosc = haslo.length;
 
+var litery = new Array(35);
+
+var skuchy = 0;
+
+litery[0] = "A";
+litery[1] = "Ą";
+litery[2] = "B";
+litery[3] = "C";
+litery[4] = "Ć";
+litery[5] = "D";
+litery[6] = "E";
+litery[7] = "Ę";
+litery[8] = "F";
+litery[9] = "G";
+litery[10] = "H";
+litery[11] = "I";
+litery[12] = "J";
+litery[13] = "K";
+litery[14] = "L";
+litery[15] = "Ł";
+litery[16] = "M";
+litery[17] = "N";
+litery[18] = "Ń";
+litery[19] = "O";
+litery[20] = "Ó";
+litery[21] = "P";
+litery[22] = "Q";
+litery[23] = "R";
+litery[24] = "S";
+litery[25] = "Ś";
+litery[26] = "T";
+litery[27] = "U";
+litery[28] = "V";
+litery[29] = "W";
+litery[30] = "X";
+litery[31] = "Y";
+litery[32] = "Z";
+litery[33] = "Ż";
+litery[34] = "Ź";
+
+
 for(i=0; i<dlugosc; i++) {
 	if(haslo.charAt(i)==" ")
 		ukryte_haslo = ukryte_haslo + " ";
@@ -16,4 +57,59 @@ function set_password() {
 	document.getElementById("haslo").innerHTML = ukryte_haslo;
 }
 
-window.onload = set_password;
+function generate_letters() {
+	var tresc_diva = "";
+	
+	for(i=0; i<35; i++) {
+		tresc_diva = tresc_diva + '<div class="letter" onClick="check(' + i + ')" id="lit' + i + '">' + litery[i] + '</div>';
+		if((i+1) % 7 == 0)
+			tresc_diva = tresc_diva + '<div style="clear:both"></div>'
+	}
+	
+	document.getElementById("litery").innerHTML = tresc_diva;
+}
+
+function start() {
+	set_password();
+	generate_letters();
+}
+
+window.onload = start;
+
+function check(nr) {
+	var trafienie = false;
+	
+	for(i=0; i<dlugosc; i++)
+	{
+		if(haslo.charAt(i) == litery[nr]) {
+			ukryte_haslo = ukryte_haslo.substr(0, i) + litery[nr] + ukryte_haslo.substr(i+1);
+			trafienie = true;
+		}
+	}
+	
+	if(trafienie) {
+		var element_id = "lit" + nr;
+		document.getElementById(element_id).style.background = "#005500";
+		document.getElementById(element_id).style.color = "#00FF00";
+		document.getElementById(element_id).style.border = "3px solid #005500";
+		document.getElementById(element_id).setAttribute("onclick", ";")
+	} else {
+		skuchy = skuchy + 1;
+		document.getElementById("szubienica").innerHTML = '<img src="img/s' + skuchy + '.jpg"/>';
+		var element_id = "lit" + nr;
+		document.getElementById(element_id).style.background = "#550000";
+		document.getElementById(element_id).style.color = "#FF0000";
+		document.getElementById(element_id).style.border = "3px solid #550000";
+		document.getElementById(element_id).setAttribute("onclick", ";")
+	}
+	
+	set_password();
+	
+	if(skuchy >= 9) {
+		document.getElementById("litery").innerHTML = "Przegrana !!";
+	}
+	
+	if(haslo == ukryte_haslo) {
+		document.getElementById("litery").innerHTML = "Wygrana !!";
+	}
+}
